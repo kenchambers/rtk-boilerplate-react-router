a867830385d657900b8312c492a522f44f56a38f
nodefiend <ken@platejoy.com>
2021-05-05 22:15:07 -0700

    first commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { combineReducers } from '@reduxjs/toolkit';
+import { createBrowserHistory } from 'history'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
+import { applyMiddleware, compose, createStore } from 'redux'
+import thunk from "redux-thunk"
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+
+export const history = createBrowserHistory()
+
+const createRootReducer = (history) => combineReducers({
+  router: connectRouter(history),
+  counter: counterReducer
+})
+
+export const store = function configureStore(preloadedState) {
+  const store = createStore(
+    createRootReducer(history),
+    preloadedState, compose(applyMiddleware(routerMiddleware(history), thunk)),
+  )
+  return store
+}
+
+
--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
+import { store, history } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { ConnectedRouter } from 'connected-react-router'
+import { Route, Switch } from 'react-router' // react-router v4/v5
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={store()}>
+      <ConnectedRouter history={history}>
+        <>
+          <Switch>
+            <Route exact path="/" render={() => (<App />)} />
+            <Route exact path="/test" render={() => (<><h1>test</h1></>)} />
+            <Route render={() => (<div>Miss</div>)} />
+          </Switch>
+        </>
+      </ConnectedRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
